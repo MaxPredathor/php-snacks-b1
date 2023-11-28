9fd0d5f333f2bf7eb20ad53d42eacabd4ad3584e
MaxPredathor <max.gilli98@gmail.com>
2023-11-28 13:46:44 +0100

    Snack 1 completed

--- Snack-1/index.php
+++ Snack-1/index.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Basket</title>
 </head>
-<body>
-    
-</body>
+    <body>
+        <main>
+            <?php 
+            $partite = array(
+                array("Olimpia Milano", "Cantù", 55, 60),
+                array("Virtus Bologna", "Dinamo Sassari", 72, 68),
+                array("EA7 Emporio Armani Milano", "VL Pesaro", 89, 75),
+            );
+
+            foreach ($partite as $partita) {
+                $squadraCasa = $partita[0];
+                $squadraOspite = $partita[1];
+                $puntiCasa = $partita[2];
+                $puntiOspite = $partita[3];
+
+                echo "$squadraCasa - $squadraOspite | $puntiCasa-$puntiOspite ";
+            }
+            ?>
+        </main>
+    </body>
 </html>
